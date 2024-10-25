f4e0d5f9200f06f83195bd20c1ee48dbb2788ca3
PyJan <jan.svarcbach@gmail.com>
2024-10-25 22:12:35 +0200

    main run func with arguments

---
+++ scripts/run.py
@@ -0,0 +1,31 @@
+"""main script to run code for different purposes"""
+
+import argparse
+import subprocess
+import pathlib
+
+def main():
+    """main func, usage: python run.py --runtype build; python run.py --runtype streamlit etc."""
+    parser = argparse.ArgumentParser(description='Process various run types')
+    choices = ['build', 'streamlit', 'test_backend', 'test_usecases', 'test_db',
+               'update_db']
+    parser.add_argument('--runtype', choices=choices, help=f'Select run type from {choices}', required=True)
+    args = parser.parse_args()
+
+    match args.runtype:
+        case 'build':
+            print('in build')
+        case 'streamlit':
+            print('streamlit')
+        case 'test_backend':
+            print('in test backend')
+        case 'test_usecases':
+            print('in use cases')
+        case 'test_db':
+            print('in test db')
+        case 'update_db':
+            print('in update db')
+
+
+if __name__ == '__main__':
+    main()
